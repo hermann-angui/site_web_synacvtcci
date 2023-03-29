eaf7056e02f5824bb2e4abf61d4b1ed4cc2a441c
root <root@LAPTOP-L2B-HA.localdomain>
2023-03-29 10:59:55 +0000

    add new

---
+++ app/src/Entity/Child.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChildRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: ChildRepository::class)]
+#[ORM\Table(name: '`child`')]
+#[ORM\HasLifecycleCallbacks()]
+class Child
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true, nullable: true)]
+    private $first_name;
+
+    #[ORM\Column(type: 'string')]
+    private $last_name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $sex;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $lieu_naissance;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $created_at;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $modified_at;
+
+    #[ORM\ManyToOne(inversedBy: 'children')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Member $parent = null;
+
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+        $this->modified_at = new \DateTime();
+    }
+
+    /**
+     * Prepersist gets triggered on Insert
+     * @ORM\PrePersist
+     */
+    public function updatedTimestamps()
+    {
+        if ($this->created_at == null) {
+            $this->created_at = new \DateTime('now');
+        }
+        $this->modified_at =  new \DateTime('now');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTime $createAt): self
+    {
+        $this->created_at = $createAt;
+
+        return $this;
+    }
+
+    public function setModifiedAt(?\DateTime $modified_at): self
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTime
+    {
+        return $this->modified_at;
+    }
+
+    public function getParent(): ?Member
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?Member $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $first_name
+     * @return Child
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     * @return Child
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * @param mixed $sex
+     * @return Child
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLieuNaissance()
+    {
+        return $this->lieu_naissance;
+    }
+
+    /**
+     * @param mixed $lieu_naissance
+     * @return Child
+     */
+    public function setLieuNaissance($lieu_naissance)
+    {
+        $this->lieu_naissance = $lieu_naissance;
+        return $this;
+    }
+
+
+}
---
+++ app/src/Repository/ChildRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Child;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Child>
+ *
+ * @method Child|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Child|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Child[]    findAll()
+ * @method Child[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ChildRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Child::class);
+    }
+
+    public function add(Child $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Child $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+}
