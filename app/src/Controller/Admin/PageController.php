61479b62e07447d263a6ea87974c090bde5c55cd
root <root@LAPTOP-L2B-HA>
2024-03-29 11:10:50 +0000

    correction

--- app/src/Controller/Admin/PageController.php
+++ app/src/Controller/Admin/PageController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 #[Route('/admin')]
 class PageController extends AbstractController
