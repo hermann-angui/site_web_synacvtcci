f8737aa879f6b03cbc25dc7623a26f3886790712
root <root@LAPTOP-L2B-HA>
2024-04-09 08:52:23 +0000

    add etape column

--- app/src/Controller/Admin/MemberController.php
+++ app/src/Controller/Admin/MemberController.php
@@ -50,8 +50,7 @@ class MemberController extends AbstractController
     #[Route(path: '/verificationlist', name: 'admin_member_verification_list')]
     public function verificationList(Request $request, MemberRepository $memberRepository): Response
     {
-
-        $members = $memberRepository->findAll();
+        $members = $memberRepository->getLastNDays(5);
         return $this->render('admin/member/verification-list.html.twig', ["members" => $members]);
     }
 
--- app/src/Repository/MemberRepository.php
+++ app/src/Repository/MemberRepository.php
@@ -72,6 +72,23 @@ class MemberRepository extends ServiceEntityRepository
         return $data;
     }
 
+    public function getLastNDays(int $days){
+        $to = new \DateTime();
+        $temp = new \DateTime();
+        $from = $temp->modify("-$days days");
+        return $this->createQueryBuilder('m')
+           // ->where('m.subscription_date BETWEEN :from AND :to')
+             ->andWhere('m.subscription_date >= :from')
+             ->andWhere('m.subscription_date <= :to')
+             ->andWhere('m.etape >= 3')
+            ->setParameter('from', $from->format('Y-m-d H:i:s'))
+            ->setParameter('to',  $to->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->getResult()
+            ;
+
+    }
+
 //    public function findOneBySomeField($value): ?Member
 //    {
 //        return $this->createQueryBuilder('m')
